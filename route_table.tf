4a79a71c360a5fc841a9e19848f88665a476a2fb
aoki <popdod.ds@yahoo.ne.jp>
2021-10-04 08:19:15 +0900

    update natゲートウェイ不要の為、一旦除去

--- route_table.tf
+++ route_table.tf
@@ -21,38 +21,40 @@ resource "aws_route_table_association" "james_public_subenet_1c_rt_assoc" {
   route_table_id = aws_route_table.james_public_rt.id
 }
 
-resource "aws_route_table" "james_private_rt_1a" {
-  vpc_id = aws_vpc.james_vpc.id
+# DBインスタンスからインターネットに接続する必要がある場合に必要
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gateway_1a.id
-  }
+# resource "aws_route_table" "james_private_rt_1a" {
+#   vpc_id = aws_vpc.james_vpc.id
 
-  tags = {
-    Name = "${var.r_prefix}-private-rt-1a"
-  }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.nat_gateway_1a.id
+#   }
 
-resource "aws_route_table" "james_private_rt_1c" {
-  vpc_id = aws_vpc.james_vpc.id
+#   tags = {
+#     Name = "${var.r_prefix}-private-rt-1a"
+#   }
+# }
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gateway_1c.id
-  }
+# resource "aws_route_table" "james_private_rt_1c" {
+#   vpc_id = aws_vpc.james_vpc.id
 
-  tags = {
-    Name = "${var.r_prefix}-private-rt-1c"
-  }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.nat_gateway_1c.id
+#   }
 
-resource "aws_route_table_association" "james_private_subnet_1a_rt_assoc" {
-  subnet_id      = aws_subnet.james_private_subnet_1a.id
-  route_table_id = aws_route_table.james_private_rt_1a.id
-}
+#   tags = {
+#     Name = "${var.r_prefix}-private-rt-1c"
+#   }
+# }
 
-resource "aws_route_table_association" "james_private_subnet_1c_rt_assoc" {
-  subnet_id      = aws_subnet.james_private_subnet_1c.id
-  route_table_id = aws_route_table.james_private_rt_1c.id
-}
+# resource "aws_route_table_association" "james_private_subnet_1a_rt_assoc" {
+#   subnet_id      = aws_subnet.james_private_subnet_1a.id
+#   route_table_id = aws_route_table.james_private_rt_1a.id
+# }
+
+# resource "aws_route_table_association" "james_private_subnet_1c_rt_assoc" {
+#   subnet_id      = aws_subnet.james_private_subnet_1c.id
+#   route_table_id = aws_route_table.james_private_rt_1c.id
+# }
